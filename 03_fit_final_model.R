e95dfaa174209da1ace82253836ee0d89e3d4a38
Nick Howlett <nhowlett87@gmail.com>
2026-08-17 11:54:53 +0100

    refactoring

--- 03_fit_final_model.R
+++ 03_fit_final_model.R
@@ -1,19 +1,20 @@
 source("00_libraries_and_utils.R")
-source("02_data_prep.R")
+source("01_data_prep.R")
+conf <- config::get()
 
 
-if(tune_model) {
-  source("03_tune_model.R")
+if(conf$tune_model) {
+  source("02_tune_model.R")
 } else {
-  model_tune <- readRDS("data/processed/rf_tune.rds")
+  model_tune_results <- readRDS("data/processed/rf_tune.rds")
 }
 
 # fit best model according to PR
 
 model <- local({
   
-  params <- model_tune$best_params
-  wf <- model_tune$tune_res %>% extract_workflow()
+  params <- model_tune_results$best_params
+  wf <- model_tune_results$tune_res %>% extract_workflow()
   fit <- wf %>%
     finalize_workflow(params) %>%
     fit(data = train_data)
@@ -21,4 +22,5 @@ model <- local({
   fit
 })
 
-vip::vip(model, 40)
+
+saveRDS(model, "data/processed/rf_final_model.rds")
